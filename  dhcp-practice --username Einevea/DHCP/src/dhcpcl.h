5d9bedd9932640159272941a4b433af3821f5135
Einevea <Einevea@892dc576-9b56-11dd-bbfe-3dabce05a288>
2008-10-16 23:10:30 +0000

    Bueno, empezando con c (que no me acuerdo de nada) estoy con la comprobación de parámetros inicial y ya sabes como me gusta a mi todo lleno de métodos.

---
+++  dhcp-practice --username Einevea/DHCP/src/dhcpcl.h
@@ -0,0 +1,17 @@
+/*
+ * dhcpcl.h
+ *
+ *  Created on: 17-oct-2008
+ *      Author: dconde
+ */
+
+#ifndef DHCPCL_H_
+#define DHCPCL_H_
+
+void printParamsError(int err);
+int checkParams(int argc, const char* argv[]);
+int main(int argc, const char* argv[]);
+
+
+
+#endif /* DHCPCL_H_ */
